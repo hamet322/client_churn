fc5a2e657971a5e34fc6a652a33e17ca687d4a64
hamet322 <hamet052494@gmail.com>
2023-05-30 11:25:44 -0500

    project structure and raw data

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='churn predictiob and price sensiivity',
+    author='hamet322',
+    license='MIT',
+)
